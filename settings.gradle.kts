0f343c69c9482c096e320b701200cf73d8476a28
wkwon <rnjsdntjr26@daum.net>
2023-11-11 16:43:02 +0900

    init: 프로젝트 초기화

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "one-bailey"
+
+include(
+    "core:core-enum",
+    "core:core-api",
+    "storage:db-core",
+    "tests:api-docs",
+    "support:logging",
+    "support:monitoring",
+    "clients:client-example"
+)
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val asciidoctorConvertVersion: String by settings
+    val ktlintVersion: String by settings
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.kapt" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+                "org.asciidoctor.jvm.convert" -> useVersion(asciidoctorConvertVersion)
+                "org.jlleitschuh.gradle.ktlint" -> useVersion(ktlintVersion)
+            }
+        }
+    }
+}
