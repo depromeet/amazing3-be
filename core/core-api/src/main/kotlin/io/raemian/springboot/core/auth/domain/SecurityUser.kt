53624f7e41c82e066e94a740463820d34028123b
ManHyuk <rnanhyuk@gmail.com>
2023-11-13 23:42:16 +0900

    chore(#1): 코드 정리

--- core/core-api/src/main/kotlin/io/raemian/springboot/core/auth/domain/SecurityUser.kt
+++ core/core-api/src/main/kotlin/io/raemian/springboot/core/auth/domain/SecurityUser.kt
@@ -2,7 +2,6 @@ package io.raemian.springboot.core.auth.domain
 
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 
 data class SecurityUser(
@@ -12,15 +11,13 @@ data class SecurityUser(
     private val authorities: List<String> = listOf("ROLE_USER"),
 ) : UserDetails {
 
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        return authorities.map {
-            SimpleGrantedAuthority(it)
-        }.toMutableList()
-    }
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
+        authorities
+            .map { SimpleGrantedAuthority(it) }
+            .toMutableList()
 
 
     override fun getPassword(): String = password
-
 
     override fun getUsername(): String = email
 
@@ -33,10 +30,3 @@ data class SecurityUser(
     override fun isEnabled(): Boolean = true
 
 }
-
-
-// class SecurityGrantedAuthority : GrantedAuthority {
-//     override fun getAuthority(): String {
-//         return "ROLE_USER"
-//     }
-// }
