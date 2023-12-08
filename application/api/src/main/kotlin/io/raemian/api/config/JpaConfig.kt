b1fbb22abad9e4098c5c848bf74a7934fa1cd827
binary_ho <dfghcvb11@naver.com>
2023-12-08 22:34:49 +0900

    refactor : 중복 구현한 BaseEntity와 불필요한 JpaConfig 삭제 (#21)

--- application/api/src/main/kotlin/io/raemian/api/config/JpaConfig.kt
+++
@@ -1,8 +0,0 @@
-package io.raemian.api.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-
-@Configuration
-@EnableJpaAuditing
-class JpaConfig
