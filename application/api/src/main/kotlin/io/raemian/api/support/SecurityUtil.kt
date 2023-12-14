a5ac5bbda4af419d633711d8db357cfe82fad68a
binary_ho <dfghcvb11@naver.com>
2023-12-15 05:19:19 +0900

    refactor : SecrityUtil의 currentMemberId 메서드의 이름을 유저 객체 이름에 맞게 currentUserId로 변경 (#27)

--- application/api/src/main/kotlin/io/raemian/api/support/SecurityUtil.kt
+++ application/api/src/main/kotlin/io/raemian/api/support/SecurityUtil.kt
@@ -5,7 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder
 
 object SecurityUtil {
     // SecurityContext 에 유저 정보가 저장되는 시점
-    fun currentMemberId(): Long {
+    fun currentUserId(): Long {
         val authentication: Authentication? = SecurityContextHolder.getContext().authentication
         if (authentication == null || authentication.name == null) {
             throw RuntimeException("Security Context 에 인증 정보가 없습니다.")
