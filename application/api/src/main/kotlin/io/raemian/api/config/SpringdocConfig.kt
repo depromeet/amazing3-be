dc5a44ff7683147c9f130282fdda8a3b2995e7d5
wkwon <wkwon@ebay.com>
2023-12-19 15:30:57 +0900

    docs(#4): springdoc 설명 추가 및 JWT 추가 기능

---
+++ application/api/src/main/kotlin/io/raemian/api/config/SpringdocConfig.kt
@@ -0,0 +1,35 @@
+package io.raemian.api.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.Arrays
+
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "BANDIBOODI API 명세",
+        description = "BANDIBOODI 메인/어드밍 API 명세서",
+        version = "v1.0.0",
+    ),
+)
+@Configuration
+class SpringdocConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        val securityScheme: SecurityScheme = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER).name("Authorization")
+        val securityRequirement: SecurityRequirement = SecurityRequirement().addList("bearerAuth")
+
+        return OpenAPI()
+            .components(Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .security(Arrays.asList(securityRequirement))
+    }
+}
