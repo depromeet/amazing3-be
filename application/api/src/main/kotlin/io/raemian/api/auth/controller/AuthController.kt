cf26e9b6d92aca6d8d8323a06b43586ea7702859
ManHyuk <rnanhyuk@gmail.com>
2023-12-20 01:55:39 +0900

    feat(#50): edit current user info api

--- application/api/src/main/kotlin/io/raemian/api/auth/controller/AuthController.kt
+++ application/api/src/main/kotlin/io/raemian/api/auth/controller/AuthController.kt
@@ -1,15 +1,13 @@
 package io.raemian.api.auth.controller
 
-import io.raemian.api.auth.controller.request.SignInRequest
 import io.raemian.api.auth.controller.request.UpdateUserRequest
+import io.raemian.api.auth.controller.response.UserResponse
 import io.raemian.api.auth.domain.CurrentUser
-import io.raemian.api.auth.domain.TokenDTO
 import io.raemian.api.auth.service.AuthService
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -19,16 +17,12 @@ class AuthController(
     private val authService: AuthService,
 ) {
 
-    @Operation(summary = "로그인 API")
-    @PostMapping("/auth/sign-in")
-    fun signIn(@RequestBody signInRequest: SignInRequest): TokenDTO {
-        return authService.signIn(signInRequest.email)
-    }
-
     @Operation(summary = "토큰 유저 정보 조회 API")
     @GetMapping("/my")
-    fun my(@AuthenticationPrincipal currentUser: CurrentUser): CurrentUser {
-        return currentUser
+    fun my(@AuthenticationPrincipal currentUser: CurrentUser): ResponseEntity<UserResponse> {
+        val user = authService.getUserById(currentUser.id)
+        val response = UserResponse.of(user)
+        return ResponseEntity.ok().body(response)
     }
 
     @Operation(summary = "유저 온보딩 이후 정보 업데이트 API")
--- application/api/src/main/kotlin/io/raemian/api/auth/service/AuthService.kt
+++ application/api/src/main/kotlin/io/raemian/api/auth/service/AuthService.kt
@@ -1,12 +1,8 @@
 package io.raemian.api.auth.service
 
 import io.raemian.api.auth.domain.CurrentUser
-import io.raemian.api.auth.domain.TokenDTO
-import io.raemian.api.support.TokenProvider
 import io.raemian.storage.db.core.user.User
 import io.raemian.storage.db.core.user.UserRepository
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
@@ -17,24 +13,11 @@ import kotlin.jvm.optionals.getOrNull
 @Service
 class AuthService(
     private val userRepository: UserRepository,
-    private val authenticationManagerBuilder: AuthenticationManagerBuilder,
-    private val tokenProvider: TokenProvider,
 ) : UserDetailsService {
 
-    override fun loadUserByUsername(username: String): UserDetails {
-        val user = userRepository.findByEmail(username) ?: throw UsernameNotFoundException("not found $username")
-        return CurrentUser(
-            id = user.id!!,
-            email = user.email,
-            authorities = listOf(),
-        )
-    }
-
-    fun signIn(email: String): TokenDTO {
-        val user = userRepository.findByEmail(email) ?: throw RuntimeException("아이디 또는 비밀번호 불일치 ")
-        val token = UsernamePasswordAuthenticationToken(user.email, "", arrayListOf())
-        val authentication = authenticationManagerBuilder.`object`.authenticate(token)
-        return tokenProvider.generateTokenDto(authentication)
+    fun getUserById(id: Long): User {
+        val user = userRepository.findById(id).getOrNull() ?: throw RuntimeException("")
+        return user
     }
 
     fun update(id: Long, nickname: String, birth: LocalDate): User {
@@ -47,5 +30,14 @@ class AuthService(
         )
 
         return userRepository.save(updated)
+    }
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByEmail(username) ?: throw UsernameNotFoundException("not found $username")
+        return CurrentUser(
+            id = user.id!!,
+            email = user.email,
+            authorities = listOf(),
+        )
     }
 }
