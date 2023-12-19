08e3b3eaeb820fbef474cce8b2337299d397bef8
binary_ho <dfghcvb11@naver.com>
2023-12-20 00:42:20 +0900

    docs : 암호화 추가 가이드 작성 (#48)

--- application/api/src/test/kotlin/io/raemian/api/support/Encryptor.kt
+++ application/api/src/test/kotlin/io/raemian/api/support/Encryptor.kt
@@ -17,9 +17,10 @@ class Encryptor {
      *   secret-key: ${JASYPT_ENCRYPTION_PASSWORD}
      * ```
      *
-     * 2. password 변수 값에 password를 대입한다. (빈으로 관리하고 싶지 않아 자동 주입 넣지 않았습니다.)
-     * 3. encrypt 메서드에 ","로 구분해 암호화할 String 값을 넣으세요.
-     * 4. ENC() 가 포함된 전체 결과를 기존 값 대신에 대입하세요.
+     * 2. 환경 변수 JASYPT_ENCRYPTION_PASSWORD에 비밀번호를 대입합니다.
+     * 3. password 변수 값에 비밀번호를 대입합니다. (빈으로 관리하고 싶지 않아 자동 주입 넣지 않았습니다.)
+     * 4. encrypt 메서드에 ","로 구분해 암호화할 String 값을 넣으세요.
+     * 5. ENC() 가 포함된 전체 결과를 기존 값 대신에 대입하세요.
      *
      *
      * @see 주의 : 순수 숫자는 안 됩니다.
