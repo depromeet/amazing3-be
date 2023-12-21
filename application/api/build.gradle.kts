c9bb63a115183ea070f8947f20f3bc6f9bdcf592
wkwon <rnjsdntjr26@daum.net>
2023-12-21 18:23:15 +0900

    Revert "[feature] 암호화를 위한 Jasypt 도입과 사용 설명 테스트 작성 (#48) (#49)"
    This reverts commit 91ba43a0bc6cc034e60e06846bc5f4ede18f4008.

--- application/api/build.gradle.kts
+++ application/api/build.gradle.kts
@@ -35,7 +35,4 @@ dependencies {
 
     /* swagger */
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-
-    /* jasypt */
-    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
 }
