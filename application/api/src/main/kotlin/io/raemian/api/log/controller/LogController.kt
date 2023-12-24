81fe9454da5e76fc19592c8a920506e8bd97aec4
wkwon <rnjsdntjr26@daum.net>
2023-12-24 22:07:49 +0900

    fix: log API swagger 명세 추가

--- application/api/src/main/kotlin/io/raemian/api/log/controller/LogController.kt
+++ application/api/src/main/kotlin/io/raemian/api/log/controller/LogController.kt
@@ -2,6 +2,7 @@ package io.raemian.api.log.controller
 
 import io.raemian.api.log.LogService
 import io.raemian.api.log.controller.request.CreateSlackErrorLogRequest
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,6 +15,7 @@ class LogController(
     val logService: LogService,
 ) {
 
+    @Operation(summary = "Slack Log 생성 API")
     @PostMapping("/slack/error")
     fun createSlackErrorLog(@RequestBody createSlackErrorLogRequest: CreateSlackErrorLogRequest): ResponseEntity<Unit> {
         logService.createSlackErrorLog(createSlackErrorLogRequest)
