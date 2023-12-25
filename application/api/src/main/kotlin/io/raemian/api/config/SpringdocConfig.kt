b4a8848fcbda0fbdf2528cdfd7d5ca89bfc33208
wkwon <rnjsdntjr26@daum.net>
2023-12-25 14:01:06 +0900

    fix(#65): swagger 서버 url 값 환경 변수로 빼기

--- application/api/src/main/kotlin/io/raemian/api/config/SpringdocConfig.kt
+++ application/api/src/main/kotlin/io/raemian/api/config/SpringdocConfig.kt
@@ -1,35 +1,40 @@
 package io.raemian.api.config
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import io.swagger.v3.oas.annotations.info.Info
-import io.swagger.v3.oas.annotations.servers.Server
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@OpenAPIDefinition(
-    servers = arrayOf(Server(url = "https://www.one-bailey.o-r.kr/api")),
-    info = Info(
-        title = "BANDIBOODI API 명세",
-        description = "BANDIBOODI 메인/어드밍 API 명세서",
-        version = "v1.0.0",
-    ),
-)
 @Configuration
 class SpringdocConfig {
 
+    @Value("\${springdoc.server.url}")
+    private lateinit var url: String
+
     @Bean
     fun openAPI(): OpenAPI {
-        val securityScheme: SecurityScheme = SecurityScheme()
-            .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-            .`in`(SecurityScheme.In.HEADER).name("Authorization")
-        val securityRequirement: SecurityRequirement = SecurityRequirement().addList("bearerAuth")
-
         return OpenAPI()
-            .components(Components().addSecuritySchemes("bearerAuth", securityScheme))
-            .security(listOf(securityRequirement))
+            .components(Components().addSecuritySchemes("bearerAuth", apiSecurityScheme()))
+            .security(apiSecurityRequirementList())
+            .info(apiInfo())
+            .servers(listOf(apiServer()))
     }
+
+    private fun apiInfo() = Info()
+        .title("BANDIBOODI API 명세")
+        .description("BANDIBOODI 메인/어드밍 API 명세서")
+        .version("v1.0.0")
+
+    private fun apiServer() = Server().url(url)
+
+    private fun apiSecurityScheme() = SecurityScheme()
+        .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+        .`in`(SecurityScheme.In.HEADER).name("Authorization")
+
+    private fun apiSecurityRequirementList() = listOf(SecurityRequirement().addList("bearerAuth"))
 }
