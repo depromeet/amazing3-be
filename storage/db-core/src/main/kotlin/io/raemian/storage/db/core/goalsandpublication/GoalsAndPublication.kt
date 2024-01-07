5e4f5b57812b131a3cf4cc3bc4ad4846f9c820ce
binary_ho <dfghcvb11@naver.com>
2024-01-07 16:46:43 +0900

    refactor : GoalsAndPublication 관련 클래스 전체 삭제.. (#69)

--- storage/db-core/src/main/kotlin/io/raemian/storage/db/core/goalsandpublication/GoalsAndPublication.kt
+++
@@ -1,9 +0,0 @@
-package io.raemian.storage.db.core.goalsandpublication
-
-
-import io.raemian.storage.db.core.goal.Goal
-
-data class GoalsAndPublication(
-    val goals: List<Goal>,
-    val isGoalsPublic: Boolean,
-)
--- storage/db-core/src/main/kotlin/io/raemian/storage/db/core/goalsandpublication/GoalsAndPublicationRepository.kt
+++
@@ -1,9 +0,0 @@
-package io.raemian.storage.db.core.goalsandpublication
-
-import org.springframework.stereotype.Repository
-
-@Repository
-interface GoalsAndPublicationRepository {
-
-    fun findGoalsAndPublicationByUserName(userName: String): GoalsAndPublication
-}
