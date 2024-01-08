9717ef10e6bcfd46fcce860fcf4cd0da77eff9be
wkwon <rnjsdntjr26@daum.net>
2024-01-08 23:49:19 +0900

    fix: admin cors 적용

---
+++ application/admin/src/main/kotlin/io/raemian/admin/config/WebConfig.kt
@@ -0,0 +1,17 @@
+package io.raemian.admin.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PATCH", "DELETE")
+            .allowedHeaders("Content-Type")
+    }
+}
