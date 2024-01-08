f7a775d9d53c9733e54429b13b1ba5c40c637eff
binary_ho <dfghcvb11@naver.com>
2024-01-09 00:10:10 +0900

    feat : LifeMap Entity에 Goals 추가 (#69)

--- storage/db-core/src/main/kotlin/io/raemian/storage/db/core/lifemap/LifeMap.kt
+++ storage/db-core/src/main/kotlin/io/raemian/storage/db/core/lifemap/LifeMap.kt
@@ -1,14 +1,18 @@
 package io.raemian.api.lifemap
 
 import io.raemian.storage.db.core.BaseEntity
+import io.raemian.storage.db.core.goal.Goal
 import io.raemian.storage.db.core.user.User
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
@@ -20,6 +24,9 @@ class LifeMap(
 
     @Column(nullable = false)
     var isPublic: Boolean = true,
+
+    @OneToMany(mappedBy = "lifeMap", cascade = [CascadeType.REMOVE], fetch = FetchType.EAGER)
+    val goals: List<Goal>,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
