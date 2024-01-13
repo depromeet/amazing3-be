eda13190fa9e0293f3ae9929142380569184d75f
wkwon <wkwon@ebay.com>
2024-01-13 23:41:40 +0900

    fix(#98): default image 변경

--- admin-ui/src/pages/_app.tsx
+++ admin-ui/src/pages/_app.tsx
@@ -27,9 +27,9 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     <>
       <SeoHead />
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/bandiboodi.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/bandiboodi.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/bandiboodi.png" />
         <link rel="manifest" href="/site.webmanifest" />
         {process.env.NEXT_PUBLIC_CODENBUTTER_SITE_ID ? (
           <script src="https://buttr.dev/butter.js" data-site-id={process.env.NEXT_PUBLIC_CODENBUTTER_SITE_ID} async />
--- admin-ui/src/pages/login.tsx
+++ admin-ui/src/pages/login.tsx
@@ -8,7 +8,7 @@ const LoginPage = () => {
     <div className="flex min-h-screen bg-white items-centerw-full">
       <div className={`relative hidden w-1/2 lg:block`}>
         <GradientBg className="absolute top-0 left-0 w-full h-full" />
-        <img src="/logo.png" className="absolute w-10 h-10 top-5 left-5" alt="logo" />
+        <img src="/bandiboodi.png" className="absolute w-10 h-10 top-5 left-5" alt="logo" />
         <div className="absolute inline-flex items-center gap-1 px-3 py-2 font-semibold text-white border-2 border-white rounded-lg left-5 bottom-5">
           <Verified width={18} height={18} />
           BANDIBOODI
