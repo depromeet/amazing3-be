a372e86c0509648d758801cff58d428631e80e4a
ManHyuk <rnanhyuk@gmail.com>
2024-01-16 01:27:39 +0900

    feat(#94): add jasypt

---
+++ backend/application/admin/src/main/kotlin/io/raemian/admin/config/JasyptConfig.kt
@@ -0,0 +1,22 @@
+package io.raemian.admin.config
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfig(
+    @Value("\${JASYPT_ENCRYPTION_PASSWORD}") val password: String,
+) {
+
+    @Bean
+    fun jasyptStringEncryptor(): StandardPBEStringEncryptor {
+        val encryptor = StandardPBEStringEncryptor()
+        encryptor.setAlgorithm("PBEWithMD5AndDES")
+        encryptor.setPassword(password)
+        return encryptor
+    }
+}
