40c37e93668a4de10bdd54544b84b5ee37eccf6f
wkwon <rnjsdntjr26@daum.net>
2024-01-28 05:18:56 +0900

    feat(#150): active user 확인용 admin api 개발

--- backend/application/admin/src/main/kotlin/io/raemian/admin/dashboard/controller/response/DashboardResponse.kt
+++ backend/application/admin/src/main/kotlin/io/raemian/admin/dashboard/controller/response/DashboardResponse.kt
@@ -1,5 +1,6 @@
 package io.raemian.admin.dashboard.controller.response
 
+import io.raemian.admin.dashboard.dto.ActiveUserStatics
 import io.raemian.admin.dashboard.dto.GoalStatics
 import io.raemian.admin.dashboard.dto.TaskStatics
 import io.raemian.admin.dashboard.dto.UserStatics
@@ -8,14 +9,16 @@ data class DashboardResponse(
     val userStatics: UserStatics,
     val goalStatics: GoalStatics,
     val taskStatics: TaskStatics,
+    val activeUserStatics: ActiveUserStatics,
 ) {
     companion object {
         fun from(
             userStatics: UserStatics,
             goalStatics: GoalStatics,
             taskStatics: TaskStatics,
+            activeUserStatics: ActiveUserStatics,
         ): DashboardResponse {
-            return DashboardResponse(userStatics, goalStatics, taskStatics)
+            return DashboardResponse(userStatics, goalStatics, taskStatics, activeUserStatics)
         }
     }
 }
---
+++ backend/application/admin/src/main/kotlin/io/raemian/admin/dashboard/dto/ActiveUserStatics.kt
@@ -0,0 +1,7 @@
+package io.raemian.admin.dashboard.dto
+
+data class ActiveUserStatics(
+    val perTodayPercent: Double,
+    val perWeekPercent: Double,
+    val perMonthPercent: Double,
+)
