4e897e40ae3b81100ba1d223fbe6f7f361c049aa
wkwon <rnjsdntjr26@daum.net>
2024-01-27 22:44:16 +0900

    feat(#150): UserLoginLog 엔티티 생성

---
+++ backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/log/LogLoginLog.kt
@@ -0,0 +1,24 @@
+package io.raemian.storage.db.core.log
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "USER_LOGIN_LOG")
+class UserLoginLog(
+
+    @Column
+    val userId: Long,
+
+    @Column
+    val latestLoginAt: LocalDateTime,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+)
