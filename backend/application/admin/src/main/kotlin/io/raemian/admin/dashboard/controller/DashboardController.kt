7c1dbc10669235047683e2b6a5f96d88f6ae43de
권우석 (Woosuk Kwon) <rnjsdntjr26@daum.net>
2024-01-28 05:23:14 +0900

    [🌎 Feature] X: 최근 로그인 시점 / Y: 사용자 수 Admin 그래프 통계 추가 (#159)
    * feat(#150): UserLoginLog 엔티티 생성
    
    * feat(#150): UserLoginLog Repository 추가
    
    * feat(#150): 유저 로그인 시점 로그 추가
    
    * fix: UserLoginLog 파일 명 수정
    
    * feat(#150): active user 확인용 admin api 개발

--- backend/application/admin/src/main/kotlin/io/raemian/admin/dashboard/controller/DashboardController.kt
+++ backend/application/admin/src/main/kotlin/io/raemian/admin/dashboard/controller/DashboardController.kt
@@ -7,9 +7,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.net.URI
-
-fun String.toUri(): URI = URI.create(this)
 
 @RestController
 @RequestMapping("/dashboard")
