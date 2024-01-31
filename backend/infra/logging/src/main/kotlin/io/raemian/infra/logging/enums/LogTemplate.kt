4699f092dc087516447adeb5911b5aada0160511
wkwon <rnjsdntjr26@daum.net>
2024-01-31 21:43:21 +0900

    feat(#164): Bandiboodi 서비스 통계 슬랙 알림 스케줄러 작업

--- backend/infra/logging/src/main/kotlin/io/raemian/infra/logging/enums/LogTemplate.kt
+++ backend/infra/logging/src/main/kotlin/io/raemian/infra/logging/enums/LogTemplate.kt
@@ -2,10 +2,12 @@ package io.raemian.infra.logging.enums
 
 enum class LogTemplate(val message: String) {
     ERROR_HEADER(":red_circle: [%s] 오류 발생"),
+    SERVICE_STATICS_HEADER(":bar_chart: %s 반디부디 서비스 현황"),
     ERROR_MESSAGE("Error Message"),
     APP_NAME("*App Name*: %s"),
     USERAGENT("*User Agent*: %s"),
     REFERER("*Referer*: %s"),
+    STATICS_INCREASE("총: %s (오늘 %s 증가)"),
     ;
 
     fun of(vararg value: String?): String {
--- backend/infra/logging/src/main/kotlin/io/raemian/infra/logging/logger/SlackLogger.kt
+++ backend/infra/logging/src/main/kotlin/io/raemian/infra/logging/logger/SlackLogger.kt
@@ -29,6 +29,8 @@ import io.raemian.infra.logging.enums.LogTemplate.USERAGENT
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Component
 class SlackLogger(
@@ -36,6 +38,40 @@ class SlackLogger(
 ) {
     @Value("\${slack.webhook.url}")
     private lateinit var url: String
+
+    @Value("\${slack.webhook.dashboard-url}")
+    private lateinit var dashboardUrl: String
+
+    fun sendDashboard(
+        totalUser: String,
+        todayIncreasedUser: String,
+        totalGoal: String,
+        todayIncreasedGoal: String,
+        totalTask: String,
+        todayIncreasedTask: String,
+        todayLoginUserRate: String,
+    ) {
+        val now = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일(E)"))
+
+        slack.send(
+            dashboardUrl,
+            payload { p: PayloadBuilder ->
+                p.text(LogTemplate.SERVICE_STATICS_HEADER.of(now))
+                    .blocks(
+                        asBlocks(
+                            header { header ->
+                                header.text(plainText(LogTemplate.SERVICE_STATICS_HEADER.of(now)))
+                            },
+                            divider(),
+                            createRichText("유저", LogTemplate.STATICS_INCREASE.of(totalUser, todayIncreasedUser)),
+                            createRichText("목표", LogTemplate.STATICS_INCREASE.of(totalGoal, todayIncreasedGoal)),
+                            createRichText("세부 목표", LogTemplate.STATICS_INCREASE.of(totalTask, todayIncreasedTask)),
+                            createRichText("오늘 접속 유저 비율", todayLoginUserRate),
+                        ),
+                    )
+            },
+        )
+    }
 
     @Async
     fun error(
@@ -61,7 +97,7 @@ class SlackLogger(
         slack.send(
             url,
             payload { p: PayloadBuilder ->
-                p.text(LogTemplate.ERROR_HEADER.of(errorLocation.value))
+                p.text(ERROR_HEADER.of(errorLocation.value))
                     .blocks(
                         asBlocks(
                             createHeaderBlock(errorLocation),
@@ -71,7 +107,6 @@ class SlackLogger(
                             createRefererBlock(referer),
                             createErrorMessageBlock(errorMessage),
                             divider(),
-
                         ),
                     )
             },
@@ -145,6 +180,29 @@ class SlackLogger(
                         section.elements(
                             asRichTextElements(
                                 Text.builder().text(errorMessage).build(),
+                            ),
+                        )
+                    },
+                ),
+            )
+        }
+    }
+
+    private fun createRichText(title: String, message: String): RichTextBlock {
+        return richText { richText ->
+            richText.elements(
+                asElements(
+                    richTextSection { section ->
+                        section.elements(
+                            asRichTextElements(
+                                Text.builder().text("- $title").style(TextStyle.builder().bold(true).build()).build(),
+                            ),
+                        )
+                    },
+                    richTextPreformatted { section ->
+                        section.elements(
+                            asRichTextElements(
+                                Text.builder().text(message).build(),
                             ),
                         )
                     },
