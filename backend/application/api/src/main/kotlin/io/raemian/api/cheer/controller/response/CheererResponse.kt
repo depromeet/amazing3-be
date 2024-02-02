87559333ec7281bc971df66ba6416275f69cd118
wkwon <rnjsdntjr26@daum.net>
2024-02-02 19:19:54 +0900

    fix: 응원단 조회 API Response  NickName 필드 추가

--- backend/application/api/src/main/kotlin/io/raemian/api/cheer/controller/response/CheererResponse.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/cheer/controller/response/CheererResponse.kt
@@ -6,15 +6,16 @@ import java.time.LocalDateTime
 data class CheererResponse(
     val userId: Long,
     val userName: String,
+    val userNickName: String,
     val userImageUrl: String,
     val cheeringAt: LocalDateTime?,
 ) {
     companion object {
         fun from(cheerer: Cheerer): CheererResponse {
             return if (cheerer.user == null) {
-                CheererResponse(-1, "", "", cheerer.cheeringAt)
+                CheererResponse(-1, "", "", "", cheerer.cheeringAt)
             } else {
-                CheererResponse(cheerer.user!!.id!!, cheerer.user!!.username!!, cheerer.user!!.image, cheerer.cheeringAt)
+                CheererResponse(cheerer.user!!.id!!, cheerer.user!!.username!!, cheerer.user!!.nickname!!, cheerer.user!!.image, cheerer.cheeringAt)
             }
         }
     }
