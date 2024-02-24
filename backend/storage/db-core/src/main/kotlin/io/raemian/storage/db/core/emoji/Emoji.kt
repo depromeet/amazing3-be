4e701de06f5bb8e2959e1cb9eb97e6109a0df090
binary_ho <dfghcvb11@naver.com>
2024-02-25 02:11:48 +0900

    feat : 이모지와 ReactedEmoji 엔티티 구현 (#190)

---
+++ backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/emoji/Emoji.kt
@@ -0,0 +1,33 @@
+package io.raemian.storage.db.core.emoji
+
+import io.raemian.storage.db.core.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.Nationalized
+
+@Entity
+@Table(name = "EMOJIS")
+class Emoji(
+    @Column(nullable = false)
+    @Nationalized
+    var name: String,
+
+    @Column(nullable = false)
+    var url: String,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+) : BaseEntity() {
+    fun updateNameAndUrl(
+        name: String,
+        url: String,
+    ) {
+        this.name = name
+        this.url = url
+    }
+}
---
+++ backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/emoji/ReactedEmoji.kt
@@ -0,0 +1,38 @@
+package io.raemian.storage.db.core.emoji
+
+import io.raemian.storage.db.core.BaseEntity
+import io.raemian.storage.db.core.goal.Goal
+import io.raemian.storage.db.core.user.User
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
+
+@Entity
+@Table(
+    name = "REACTED_EMOJIS",
+    uniqueConstraints = [
+        UniqueConstraint(name = "uk_reacted_emoji", columnNames = ["goal_id", "react_user_id", "emoji_id"])
+    ]
+)
+class ReactedEmoji(
+    @ManyToOne
+    @JoinColumn(name = "goal_id")
+    val goal: Goal,
+
+    @ManyToOne
+    @JoinColumn(name = "emoji_id")
+    val emoji: Emoji,
+
+    @ManyToOne
+    @JoinColumn(name = "react_user_id")
+    val reactUser: User,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+) : BaseEntity()
