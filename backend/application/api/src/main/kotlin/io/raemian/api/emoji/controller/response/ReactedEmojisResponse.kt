47d6ea276826720a6bc67973ca369240f9368ea2
binary_ho <dfghcvb11@naver.com>
2024-02-28 00:13:42 +0900

    [🌎 Refactor] Goal 이모지 반응하기 API 정정 (#193)
    * refactor : reactedEmojis를 json 배열의 형태로 응답 (#192)
    
    * refactor : ReactedEmojisResponse 변수명 정리 (#192)
    
    * refactor : ReactUser 응답에 id가 아닌 username을 응답해 주도록 변경  (#192)
    
    * refactor : 정적 팩터리 메서드의 이름을 컨벤션에 맞게 of로 변경 (#192)
    
    * refactor : ReactUser를 정적 팩터리 메서드를 통해 만들도록 변경 (#192)
    
    * feat : Reaction이 요청을 보낸 본인 것인지 확인하는 필드 isMyReaction 추가 (#192)
    
    * feat : 가장 최근에 반응한 유저의 nickname을 응답에 포함 (#192)
    
    * refactor : ReactedEmojis 조회시 "내가 누른 이모지" 확인을 위한 로그인한 user의 username 전달 (#192)
    
    * ktlint formatting (#192)
    
    * test : 코드 변경에 따라 테스트 코드 변경 (#192)
    
    * refactor : 불필요한 코틀린 스트림 제거 (#192)
    
    * refactor : ReactUser를 Set이 아닌 List로 관리 (#192)
    
    * refactor : 불필요한 문법 제거 (#192)

--- backend/application/api/src/main/kotlin/io/raemian/api/emoji/controller/response/ReactedEmojisResponse.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/emoji/controller/response/ReactedEmojisResponse.kt
@@ -5,61 +5,78 @@ import io.raemian.storage.db.core.user.User
 
 data class ReactedEmojisResponse(
     val totalReactedEmojisCount: Int,
-    val reactedEmojis: Map<Long, ReactedEmojiDTO>,
+    val latestReactUserNickname: String?,
+    val reactedEmojis: List<ReactedEmojiAndReactUsers>,
 ) {
     companion object {
-        fun of(reactedEmojis: List<ReactedEmoji>): ReactedEmojisResponse {
-            val mapValues = reactedEmojis
-                .filter { it.emoji.id != null }
-                .groupBy { it.emoji.id!! }
-                .mapValues(ReactedEmojiDTO.Companion::from)
+        fun of(reactedEmojis: List<ReactedEmoji>, username: String): ReactedEmojisResponse {
+            val reactedEmojiAndReactUsers = convert(reactedEmojis, username)
 
-            val totalEmojisCount = mapValues.values.sumOf { it.count }
-            return ReactedEmojisResponse(totalEmojisCount, mapValues)
+            val totalEmojisCount = reactedEmojiAndReactUsers.sumOf { it.reactCount }
+            val latestReactUserNickname = reactedEmojis.lastOrNull()?.reactUser?.nickname
+            return ReactedEmojisResponse(
+                totalReactedEmojisCount = totalEmojisCount,
+                latestReactUserNickname = latestReactUserNickname,
+                reactedEmojis = reactedEmojiAndReactUsers,
+            )
         }
+
+        private fun convert(
+            reactedEmojis: List<ReactedEmoji>,
+            username: String,
+        ): List<ReactedEmojiAndReactUsers> =
+            reactedEmojis
+                .filter { it.emoji.id != null }
+                .groupBy { it.emoji.id }
+                .mapValues { entry -> ReactedEmojiAndReactUsers.of(entry.value, username) }
+                .values
+                .toList()
     }
 
-    data class ReactedEmojiDTO(
+    data class ReactedEmojiAndReactUsers(
         val id: Long?,
         val name: String,
         val url: String,
-        val count: Int,
-        val reactUsers: Set<ReactUserDTO>,
+        val reactCount: Int,
+        val isMyReaction: Boolean,
+        val reactUsers: List<ReactUser>,
     ) {
         companion object {
-            fun from(reactedEmojis2: Map.Entry<Long, List<ReactedEmoji>>): ReactedEmojiDTO {
-                val reactedEmojis = reactedEmojis2.value
+            fun of(reactedEmojis: List<ReactedEmoji>, username: String): ReactedEmojiAndReactUsers {
                 val emoji = reactedEmojis.first().emoji
                 val reactUsers = getReactUsers(reactedEmojis)
+                val isMyReaction = reactUsers.any { it.username == username }
 
-                return ReactedEmojiDTO(
+                return ReactedEmojiAndReactUsers(
                     id = emoji.id,
                     name = emoji.name,
                     url = emoji.url,
-                    count = reactUsers.size,
+                    reactCount = reactUsers.size,
+                    isMyReaction = isMyReaction,
                     reactUsers = reactUsers,
                 )
             }
 
             private fun getReactUsers(reactedEmojis: List<ReactedEmoji>) =
-                reactedEmojis.stream()
+                reactedEmojis
                     .map { it.reactUser }
+                    .filter { it.username != null }
                     .filter { it.nickname != null }
-                    .map(ReactedEmojisResponse::ReactUserDTO)
-                    .toList()
-                    .toSet()
+                    .map(ReactUser::from)
         }
     }
 
-    data class ReactUserDTO(
-        val id: Long,
+    data class ReactUser(
+        val username: String,
         val nickname: String,
         val image: String,
     ) {
-        constructor(user: User) : this(
-            id = user.id!!,
-            nickname = user.nickname!!,
-            image = user.image,
-        )
+        companion object {
+            fun from(user: User) = ReactUser(
+                username = user.username!!,
+                nickname = user.nickname!!,
+                image = user.image,
+            )
+        }
     }
 }
--- backend/application/api/src/test/kotlin/io/raemian/api/integration/emoji/EmojiServiceTest.kt
+++ backend/application/api/src/test/kotlin/io/raemian/api/integration/emoji/EmojiServiceTest.kt
@@ -113,7 +113,7 @@ class EmojiServiceTest {
         reactedEmojiRepository.saveAll(listOf(reactedEmoji, reactedEmoji2, reactedEmoji3))
 
         // when
-        val reactedEmojis = emojiService.findAllReactedEmojisByGoalId(GOAL_FIXTURE.id!!)
+        val reactedEmojis = emojiService.findAllReactedEmojisByGoalId(GOAL_FIXTURE.id!!, USER_FIXTURE.username!!)
 
         // then
         assertThat(reactedEmojis.totalReactedEmojisCount).isEqualTo(3)
