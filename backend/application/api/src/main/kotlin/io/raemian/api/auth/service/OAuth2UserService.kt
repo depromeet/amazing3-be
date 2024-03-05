d137defd3e532390ad0f6f3bf53d490a7e51a4ab
ManHyuk <rnanhyuk@gmail.com>
2024-03-05 22:32:53 +0900

    feat(#198): edit nickname

--- backend/application/api/src/main/kotlin/io/raemian/api/auth/service/OAuth2UserService.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/auth/service/OAuth2UserService.kt
@@ -74,13 +74,15 @@ class OAuth2UserService(
         )
 
         userRepository.save(user)
-        val updateUsername = updateUsername(user)
+        val updateUsername = updateUsernameAndNickname(user)
         createUserDefaultLifeMap(user)
         return updateUsername
     }
 
-    private fun updateUsername(user: User): User {
-        val updateUsername = user.updateUsername("$USERNAME_PREFIX${user.id!!}")
+    private fun updateUsernameAndNickname(user: User): User {
+        val updateUsername = user
+            .updateUsername("$USERNAME_PREFIX${user.id!!}")
+            .updateNickname("$USERNAME_PREFIX${user.id!!}")
         return userRepository.save(updateUsername)
     }
 
