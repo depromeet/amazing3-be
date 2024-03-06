6d7b411b10a8ab84a1c16cbf8669d1ef0aa946ab
binary_ho <dfghcvb11@naver.com>
2024-03-06 21:22:23 +0900

    refactor : 불필요한 트랜잭션 전파 및 Transactional 이벤트 리스너 제거 (#196)

--- backend/application/api/src/main/kotlin/io/raemian/api/comment/event/UpdateCommentReadAtEventHandler.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/comment/event/UpdateCommentReadAtEventHandler.kt
@@ -1,22 +1,21 @@
 package io.raemian.api.comment.event
 
 import io.raemian.storage.db.core.goal.GoalRepository
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.transaction.event.TransactionPhase
-import org.springframework.transaction.event.TransactionalEventListener
 
 @Service
 class UpdateCommentReadAtEventHandler(
     private val goalRepository: GoalRepository,
 ) {
     @Async
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    fun updateLastCommentReadTime(event: UpdateLastCommentReadAtEvent) =
+    @Transactional
+    @EventListener
+    fun updateLastCommentReadTime(event: UpdateLastCommentReadAtEvent) {
         goalRepository.updateLastCommentReadAtByGoalId(
             event.goalId, event.commentReadAt,
         )
+    }
 }
