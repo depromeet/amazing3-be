963a7755328e51808d1f48505e0b270e8686d560
binary_ho <dfghcvb11@naver.com>
2024-03-06 10:11:28 +0900

    feat : Goal 댓글 작성 삭제 메서드 구현 (#196)

--- backend/application/api/src/main/kotlin/io/raemian/api/comment/CommentService.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/comment/CommentService.kt
@@ -1,12 +1,19 @@
 package io.raemian.api.comment
 
+import io.raemian.api.support.error.CoreApiException
+import io.raemian.api.support.error.ErrorInfo
+import io.raemian.storage.db.core.comment.Comment
 import io.raemian.storage.db.core.comment.CommentRepository
+import io.raemian.storage.db.core.goal.GoalRepository
+import io.raemian.storage.db.core.user.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CommentService(
     val commentRepository: CommentRepository,
+    val goalRepository: GoalRepository,
+    val userRepository: UserRepository,
 ) {
 
     companion object {
@@ -16,4 +23,30 @@ class CommentService(
     @Transactional(readOnly = true)
     fun getAllByGoalId(goalId: Long, currentUserId: Long) =
         CommentsResponse.from(commentRepository.findAllByGoalId(goalId), currentUserId)
+
+    @Transactional
+    fun write(goalId: Long, currentUserId: Long, content: String) {
+        validateContentCharacterLimit(content)
+
+        val goal = goalRepository.getReferenceById(goalId)
+        val currentUser = userRepository.getReferenceById(currentUserId)
+        val comment = Comment(goal, currentUser, content)
+        commentRepository.save(comment)
+    }
+
+    @Transactional
+    fun delete(commentId: Long, currentUserId: Long) {
+        val comment = commentRepository.getById(commentId)
+        if (currentUserId != comment.commenter.id) {
+            throw CoreApiException(ErrorInfo.RESOURCE_DELETE_FORBIDDEN)
+        }
+
+        commentRepository.delete(comment)
+    }
+
+    private fun validateContentCharacterLimit(content: String) {
+        if (content.length > CONTENT_CHARACTER_LIMIT) {
+            throw CoreApiException(ErrorInfo.COMMENT_CHARACTER_LIMIT_EXCEED)
+        }
+    }
 }
