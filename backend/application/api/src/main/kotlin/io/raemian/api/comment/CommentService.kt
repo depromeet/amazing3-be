da68e9a590a862f06e2db599a16753909bc3c922
binary_ho <dfghcvb11@naver.com>
2024-03-06 10:47:59 +0900

    feat : Goal의 생성자가 읽지 않은 댓글이 있는지 확인하는 메서드 구현 (#196)

--- backend/application/api/src/main/kotlin/io/raemian/api/comment/CommentService.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/comment/CommentService.kt
@@ -4,16 +4,20 @@ import io.raemian.api.support.error.CoreApiException
 import io.raemian.api.support.error.ErrorInfo
 import io.raemian.storage.db.core.comment.Comment
 import io.raemian.storage.db.core.comment.CommentRepository
+import io.raemian.storage.db.core.comment.GoalCommentReadTime
+import io.raemian.storage.db.core.comment.GoalCommentReadTimeRepository
 import io.raemian.storage.db.core.goal.GoalRepository
 import io.raemian.storage.db.core.user.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Service
 class CommentService(
     val commentRepository: CommentRepository,
     val goalRepository: GoalRepository,
     val userRepository: UserRepository,
+    val goalCommentReadTimeRepository: GoalCommentReadTimeRepository,
 ) {
 
     companion object {
@@ -23,6 +27,15 @@ class CommentService(
     @Transactional(readOnly = true)
     fun getAllByGoalId(goalId: Long, currentUserId: Long) =
         CommentsResponse.from(commentRepository.findAllByGoalId(goalId), currentUserId)
+
+    @Transactional
+    fun isNewComment(goalId: Long): Boolean =
+        goalCommentReadTimeRepository.findByGoalId(goalId)?.let {
+            return commentRepository.existsByCreatedAtGreaterThan(it.lastCommentReadTime)
+        } ?: run {
+            createNewGoalCommentReadTime(goalId)
+            return true
+        }
 
     @Transactional
     fun write(goalId: Long, currentUserId: Long, content: String) {
@@ -42,6 +55,12 @@ class CommentService(
         }
 
         commentRepository.delete(comment)
+    }
+
+    private fun createNewGoalCommentReadTime(goalId: Long) {
+        val goal = goalRepository.getReferenceById(goalId)
+        val goalCommentReadTime = GoalCommentReadTime(goal, LocalDateTime.now())
+        goalCommentReadTimeRepository.save(goalCommentReadTime)
     }
 
     private fun validateContentCharacterLimit(content: String) {
--- backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/comment/CommentRepository.kt
+++ backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/comment/CommentRepository.kt
@@ -1,10 +1,13 @@
 package io.raemian.storage.db.core.comment
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
 interface CommentRepository : JpaRepository<Comment, Long> {
 
     fun findAllByGoalId(goalId: Long): List<Comment>
+
+    fun existsByCreatedAtGreaterThan(createdAt: LocalDateTime): Boolean
 
     override fun getById(id: Long): Comment =
         findById(id).orElseThrow() { NoSuchElementException("Comment가 없습니다 $id") }
