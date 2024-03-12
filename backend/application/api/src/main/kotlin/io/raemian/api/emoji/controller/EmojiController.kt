d76f62f3b6ca99149443370ad52c94fc80ba8a34
binary_ho <dfghcvb11@naver.com>
2024-03-13 00:18:02 +0900

    refactor : Emoji API Restful하게 개선 (#196)

--- backend/application/api/src/main/kotlin/io/raemian/api/emoji/controller/EmojiController.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/emoji/controller/EmojiController.kt
@@ -2,8 +2,6 @@ package io.raemian.api.emoji.controller
 
 import io.raemian.api.auth.domain.CurrentUser
 import io.raemian.api.emoji.EmojiService
-import io.raemian.api.emoji.controller.request.ReactEmojiRequest
-import io.raemian.api.emoji.controller.request.RemoveEmojiRequest
 import io.raemian.api.emoji.controller.response.EmojiResponse
 import io.raemian.api.emoji.controller.response.ReactedEmojisResponse
 import io.raemian.api.support.response.ApiResponse
@@ -14,25 +12,22 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/emoji")
 class EmojiController(
     private val emojiService: EmojiService,
 ) {
 
     @Operation(summary = "이모지 전체 조회 API")
-    @GetMapping
+    @GetMapping("/emoji")
     fun findAll(): ResponseEntity<ApiResponse<List<EmojiResponse>>> =
         ResponseEntity.ok(
             ApiResponse.success(emojiService.findAll()),
         )
 
     @Operation(summary = "Goal에 반응된 이모지와 유저 정보 전체 조회 API")
-    @GetMapping("/{goalId}")
+    @GetMapping("/goal/{goalId}/emoji")
     fun findAllReactedEmojisAtGoal(
         @PathVariable goalId: Long,
         @AuthenticationPrincipal currentUser: CurrentUser,
@@ -42,24 +37,24 @@ class EmojiController(
     }
 
     @Operation(summary = "Goal에 이모지 반응하기 API")
-    @PostMapping("/{goalId}")
+    @PostMapping("/goal/{goalId}/emoji/{emojiId}")
     fun react(
-        @RequestBody reactEmojiRequest: ReactEmojiRequest,
         @PathVariable goalId: Long,
+        @PathVariable emojiId: Long,
         @AuthenticationPrincipal currentUser: CurrentUser,
     ): ResponseEntity<ApiResponse<Unit>> {
-        emojiService.react(reactEmojiRequest.emojiId, goalId, currentUser.id)
+        emojiService.react(emojiId, goalId, currentUser.id)
         return ResponseEntity.ok(ApiResponse.success())
     }
 
     @Operation(summary = "Goal에 반응한 이모지 삭제 API")
-    @DeleteMapping("/{goalId}")
+    @DeleteMapping("/goal/{goalId}/emoji/{emojiId}")
     fun remove(
-        @RequestBody removeEmojiRequest: RemoveEmojiRequest,
         @PathVariable goalId: Long,
+        @PathVariable emojiId: Long,
         @AuthenticationPrincipal currentUser: CurrentUser,
     ): ResponseEntity<ApiResponse<Unit>> {
-        emojiService.remove(removeEmojiRequest.emojiId, goalId, currentUser.id)
+        emojiService.remove(emojiId, goalId, currentUser.id)
         return ResponseEntity.ok(ApiResponse.success())
     }
 }
