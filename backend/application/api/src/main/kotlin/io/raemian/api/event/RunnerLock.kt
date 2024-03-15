fbfbdecbca7fbdb740c21734ecf48e27cc15c20e
권우석 (Woosuk Kwon) <rnjsdntjr26@daum.net>
2024-03-15 17:00:01 +0900

    [🛠️ Refactor] RunnerLock 필드 변수명 변경 + Security Uri 상수화 + H2 웹 콘솔용 WebSecurityCustomizer 제거 (#226)
    * refactor: RunnerLock 내부 필드 변수명 변경
    
    * refactor: security permitall uri 상수화

--- backend/application/api/src/main/kotlin/io/raemian/api/event/RunnerLock.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/event/RunnerLock.kt
@@ -4,13 +4,13 @@ import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.locks.ReentrantLock
 
 class RunnerLock : ReentrantLock() {
-    private val watingThread = AtomicInteger(0)
+    private val watingThreadCount = AtomicInteger(0)
 
     fun increase(): Int {
-        return watingThread.addAndGet(1)
+        return watingThreadCount.addAndGet(1)
     }
 
     fun decrease(): Int {
-        return watingThread.decrementAndGet()
+        return watingThreadCount.decrementAndGet()
     }
 }
