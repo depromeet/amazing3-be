b289cab1668e1e4c7088dc501cb9bfaf6ad67143
ManHyuk <rnanhyuk@gmail.com>
2024-03-17 14:27:12 +0900

    feat(#232): add title field

--- backend/application/api/src/main/kotlin/io/raemian/api/lifemap/domain/GoalDto.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/lifemap/domain/GoalDto.kt
@@ -5,13 +5,17 @@ import io.raemian.storage.db.core.goal.Goal
 
 class GoalDto private constructor(
     val id: Long?,
+    val title: String,
+    @Deprecated("필드 삭제 예정")
     val deadline: String,
     val stickerUrl: String,
+    @Deprecated("필드 삭제 예정")
     val tagContent: String,
 ) {
 
     constructor(goal: Goal) : this(
         id = goal.id,
+        title = goal.title,
         deadline = goal.deadline.format(),
         stickerUrl = goal.sticker.url,
         tagContent = goal.tag.content,
