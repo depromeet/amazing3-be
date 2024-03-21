cd4a1874587335686707e5ffd478e5412e5ba04a
binary_ho <dfghcvb11@naver.com>
2024-03-21 10:22:28 +0900

    [🛠️ Refactor] 잘못 작성된 읽지 않은 댓글 조회 쿼리 올바르게 정정 (#237)
    * refactor : CommentsResponse에 자신의 목표인지 확인하는 isMyGoal 필드 추가 (#229)
    
    * test : CommentsResponse를 통해 자신의 목표인지 확인할 수 있는지 검증하는 테스트 코드 작성 (#229)
    
    * refactor : exist new comment 쿼리를 id와 시간으로 검색하도록 변경 (#236)
    
    * refactor : 쿼리가 Goal Id를 사용하도록 변경 (#236)
    
    * test : 본인의 Goal만 조회하는지 검증하는 테스트 추가 작성 (#236)

--- backend/application/api/src/main/kotlin/io/raemian/api/comment/service/CommentService.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/comment/service/CommentService.kt
@@ -46,7 +46,7 @@ class CommentService(
     @Transactional
     fun isNewComment(goalId: Long): Boolean {
         val goal = goalRepository.getById(goalId)
-        return commentRepository.existsByCreatedAtGreaterThan(goal.lastCommentReadAt)
+        return commentRepository.existsByGoalIdAndCreatedAtGreaterThan(goal.id!!, goal.lastCommentReadAt)
     }
 
     @Transactional
