0c95f367b7f7a5d2b29dbb2b52d7f4996bc61f88
wkwon <rnjsdntjr26@daum.net>
2024-03-21 23:19:20 +0900

    refactor: CheererRepository 미사용 메서드 제거

--- backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/cheer/CheererRepository.kt
+++ backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/cheer/CheererRepository.kt
@@ -1,9 +1,5 @@
 package io.raemian.storage.db.core.cheer
 
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CheererRepository : JpaRepository<Cheerer, Long> {
-
-    fun findAllByLifeMapIdAndIdLessThanOrderByIdDesc(lifeMapId: Long, id: Long, pageable: Pageable): List<Cheerer>
-}
+interface CheererRepository : JpaRepository<Cheerer, Long>
