54c8b81323fa72bf53938e6b4312d14cee6b278c
wkwon <rnjsdntjr26@daum.net>
2024-03-24 14:21:40 +0900

    refactor: PaginationResult total값 Long으로 다시 변경

--- backend/application/api/src/main/kotlin/io/raemian/api/support/response/PaginationResult.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/support/response/PaginationResult.kt
@@ -3,15 +3,24 @@ package io.raemian.api.support.response
 import io.raemian.storage.db.core.common.pagination.CursorPaginationResult
 
 data class PaginationResult<T>(
-    val total: Int,
+    val total: Long,
     val contents: List<T>,
     val isLast: Boolean,
     val nextCursor: Long?,
 ) {
     companion object {
-        fun <T> from(total: Int, result: CursorPaginationResult<T>): PaginationResult<T> {
+        fun <T> from(total: Long, result: CursorPaginationResult<T>): PaginationResult<T> {
             return PaginationResult(
                 total = total,
+                contents = result.contents,
+                isLast = result.isLast,
+                nextCursor = result.nextCursor,
+            )
+        }
+
+        fun <T> from(total: Int, result: CursorPaginationResult<T>): PaginationResult<T> {
+            return PaginationResult(
+                total = total.toLong(),
                 contents = result.contents,
                 isLast = result.isLast,
                 nextCursor = result.nextCursor,
