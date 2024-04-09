c1a50734e405cfbbcacd786db2d3e0e973f533ea
binary_ho <dfghcvb11@naver.com>
2024-04-09 11:32:08 +0900

    feat : 이모지 추가 전에 같은 이모지가 존재하는지 확인하는 메서드 작성 (#253)

--- backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/emoji/ReactedEmojiRepository.kt
+++ backend/storage/db-core/src/main/kotlin/io/raemian/storage/db/core/emoji/ReactedEmojiRepository.kt
@@ -5,6 +5,9 @@ import io.raemian.storage.db.core.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ReactedEmojiRepository : JpaRepository<ReactedEmoji, Long> {
+
+    fun existsByReactUserIdAndGoalIdAndEmojiId(reactUserId: Long, goalId: Long, emojiId: Long): Boolean
+
     fun findAllByGoal(goal: Goal): List<ReactedEmoji>
 
     fun findAllByGoalIdIn(goalIds: List<Long>): List<ReactedEmoji>
