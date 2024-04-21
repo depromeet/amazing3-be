6495aff228b0aaec0ed0c7fced6c33ae4274ad44
binary_ho <dfghcvb11@naver.com>
2024-04-21 15:15:24 +0900

    feat : 이모지 추가 전에 같은 이모지가 존재하는지 확인하는 메서드 작성 (#253) (#256)

--- backend/application/api/src/main/kotlin/io/raemian/api/emoji/service/EmojiService.kt
+++ backend/application/api/src/main/kotlin/io/raemian/api/emoji/service/EmojiService.kt
@@ -4,14 +4,12 @@ import io.raemian.api.emoji.model.EmojiResult
 import io.raemian.api.emoji.model.ReactedEmojisResult
 import io.raemian.api.event.model.ReactedEmojiEvent
 import io.raemian.api.event.model.RemovedEmojiEvent
-import io.raemian.storage.db.core.emoji.EmojiCountRepository
 import io.raemian.storage.db.core.emoji.EmojiRepository
 import io.raemian.storage.db.core.emoji.ReactedEmoji
 import io.raemian.storage.db.core.emoji.ReactedEmojiRepository
 import io.raemian.storage.db.core.goal.GoalRepository
 import io.raemian.storage.db.core.user.UserRepository
 import org.springframework.context.ApplicationEventPublisher
-import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -22,7 +20,6 @@ class EmojiService(
     private val userRepository: UserRepository,
     private val reactedEmojiRepository: ReactedEmojiRepository,
     private val applicationEventPublisher: ApplicationEventPublisher,
-    private val emojiCountRepository: EmojiCountRepository,
 ) {
     @Transactional(readOnly = true)
     fun findAll(): List<EmojiResult> =
@@ -41,14 +38,25 @@ class EmojiService(
         val emoji = emojiRepository.getReferenceById(emojiId)
         val goal = goalRepository.getReferenceById(goalId)
         val emojiReactUser = userRepository.getReferenceById(emojiReactUserId)
-        val reactedEmoji = ReactedEmoji(goal, emoji, emojiReactUser)
 
-        ignoreDuplicatedReactedEmojiException {
+        if (isReactedEmojiNotExist(
+                reactUserId = emojiReactUserId,
+                goalId = goalId,
+                emojiId = emojiId,
+            )
+        ) {
+            val reactedEmoji = ReactedEmoji(goal, emoji, emojiReactUser)
             reactedEmojiRepository.save(reactedEmoji)
+            applicationEventPublisher.publishEvent(ReactedEmojiEvent(goalId, emojiId))
         }
-
-        applicationEventPublisher.publishEvent(ReactedEmojiEvent(goalId, emojiId))
     }
+
+    private fun isReactedEmojiNotExist(reactUserId: Long, goalId: Long, emojiId: Long) =
+        reactedEmojiRepository.existsByReactUserIdAndGoalIdAndEmojiId(
+            reactUserId = reactUserId,
+            goalId = goalId,
+            emojiId = emojiId,
+        ).not()
 
     @Transactional
     fun remove(emojiId: Long, goalId: Long, emojiReactUserId: Long) {
@@ -68,10 +76,4 @@ class EmojiService(
             .groupBy { it.goal.id!! }
             .mapValues { ReactedEmojisResult.of(it.value, userId ?: -1) }
     }
-}
-
-inline fun ignoreDuplicatedReactedEmojiException(action: () -> Any) {
-    try {
-        action()
-    } catch (exception: DataIntegrityViolationException) {}
 }
